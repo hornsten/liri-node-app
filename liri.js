c19a0c4ef47a1e50bb6a94e07328ce8dccd32151
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-12-11 09:40:07 -0600

    Able to extract text and date created from the response

--- liri.js
+++ liri.js
@@ -5,6 +5,17 @@ var client = new Twitter(keys.twitterKeys);
 
 client.get('statuses/user_timeline', { screen_name: 'jhornsten', count: 20 }, function(error, tweets, response) {
     if (error) throw error;
-    console.log(JSON.stringify(tweets, null, 2)); // The favorites. 
-    // console.log(JSON.stringify(response, null, 2)); // Raw response object. 
+    // console.log(JSON.stringify(tweets, null, 2)); // The tweets.
+
+
+
+    for (var i = 0; i < tweets.length; i++) {
+        var text = tweets[i].text;
+        var createdAt = tweets[i].created_at;
+        console.log(i + ". " + createdAt + " : " + text);
+    }
+
+
+    // console.log(JSON.stringify(response, null, 2)); // Raw response object.
+
 });
