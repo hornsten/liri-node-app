bc0619a78d7e864bcbd5cf923208fea8aedd2cc9
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-12-12 10:53:10 -0600

    Beginning to optimize

--- liri.js
+++ liri.js
@@ -24,59 +24,12 @@ function liri() {
         // If the user wants LIRI to print a list of my latest tweets
 
         if ((choice.userChoice) === 'my-tweets') {
-            client.get('statuses/user_timeline', { screen_name: 'jhornsten', count: 15 }, function(error, tweets, response) {
-                if (error) throw error;
 
-                for (var i = 0; i < tweets.length; i++) {
-                    var text = tweets[i].text;
-                    var createdAt = tweets[i].created_at;
-                    console.log('******************************************************');
-                    console.log(i + ". " + createdAt + " : " + text);
-                }
-
-            });
+            myTweets();
 
         } else if (choice.userChoice === 'spotify-this-song') {
 
-            inquirer.prompt([
-
-                {
-                    type: "input",
-                    name: "song",
-                    message: "What song would you like to know about?",
-
-                    //if user doesn't input anything, "The Sign" is the default
-                    default: function() {
-                        return 'The Sign Ace of Base';
-                    }
-                }
-
-            ]).then(function(user) {
-
-                //Search Spotify for user-specified song.
-
-                var theSong = user.song;
-
-                spotify.search({ type: 'track', query: theSong }, function(err, data) {
-
-
-                    if (err) {
-                        console.log('Error occurred: ' + err);
-                        return;
-                    }
-
-                    var songInfo = data.tracks.items[0];
-
-                    console.log('***************************************');
-                    console.log('Artist: ' + songInfo.artists[0].name);
-                    console.log('Song Title: ' + songInfo.name);
-                    console.log('Spotify Preview Link: ' + songInfo.preview_url);
-                    console.log('Album: ' + songInfo.album.name);
-                    console.log('***************************************');
-                    return;
-
-                });
-            })
+            spotifyThis();
 
         } else if (choice.userChoice === 'movie-this') {
 
@@ -118,9 +71,36 @@ function liri() {
 
                         });
                     } else if (myArray[0] === 'movie-this') {
-                        movieThis();
+                        var coolMovie = myArray[1];
+
+
+                        var queryUrl = "http://www.omdbapi.com/?t=" + coolMovie + "&y=&plot=short&tomatoes=true&r=json";
+
+                        request(queryUrl, function(error, response, body) {
+
+                            // If the request is successful
+                            if (!error && response.statusCode === 200) {
+
+                                // Parse the body of the site and print the movie info
+
+                                console.log('********************************************************');
+                                console.log("Title: " + JSON.parse(body).Title);
+                                console.log("Year: " + JSON.parse(body).Year);
+                                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                                console.log("Country: " + JSON.parse(body).Country);
+                                console.log("Language: " + JSON.parse(body).Language);
+                                console.log("Plot: " + JSON.parse(body).Plot);
+                                console.log("Actors: " + JSON.parse(body).Actors);
+                                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+                                console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
+                                console.log('********************************************************');
+
+                            }
+                        });
+
+
                     } else if (myArray[0] === 'my-tweets') {
-                        console.log('Tweet this, hoser');
+                        myTweets();
                     }
 
                 }
@@ -134,6 +114,60 @@ function liri() {
 
 }
 
+function myTweets() {
+    client.get('statuses/user_timeline', { screen_name: 'jhornsten', count: 15 }, function(error, tweets, response) {
+        if (error) throw error;
+
+        for (var i = 0; i < tweets.length; i++) {
+            var text = tweets[i].text;
+            var createdAt = tweets[i].created_at;
+            console.log('******************************************************');
+            console.log(i + ". " + createdAt + " : " + text);
+        }
+
+    });
+}
+var spotifyPrompt = {
+    type: "input",
+    name: "song",
+    message: "What song would you like to know about?",
+
+    //if user doesn't input anything, "The Sign" is the default
+    default: function() {
+        return 'The Sign Ace of Base';
+    }
+}
+
+function spotifyThis() {
+    inquirer.prompt(spotifyPrompt).then(function(user) {
+
+        //Search Spotify for user-specified song.
+
+        var theSong = user.song;
+
+        spotify.search({ type: 'track', query: theSong }, function(err, data) {
+
+
+            if (err) {
+                console.log('Error occurred: ' + err);
+                return;
+            }
+
+            var songInfo = data.tracks.items[0];
+
+            console.log('***************************************');
+            console.log('Artist: ' + songInfo.artists[0].name);
+            console.log('Song Title: ' + songInfo.name);
+            console.log('Spotify Preview Link: ' + songInfo.preview_url);
+            console.log('Album: ' + songInfo.album.name);
+            console.log('***************************************');
+            return;
+
+        });
+    })
+
+};
+
 var moviePrompt = {
     type: "input",
     name: "movie",
@@ -146,6 +180,7 @@ var moviePrompt = {
 
 
 function movieThis() {
+
     inquirer.prompt(moviePrompt).then(function(user) {
 
         // Run a request to the OMDB API with the user's movie specified. 
@@ -176,5 +211,6 @@ function movieThis() {
 
     });
 }
+
 //Start LIRI
 liri();
