4ea69317d93099aa4f4b3b7956f413d741bf6fe8
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-12-14 10:45:57 -0600

    Optimizes code

--- liri.js
+++ liri.js
@@ -27,19 +27,24 @@ function liri() {
             myTweets();
 
         } else if (choice.userChoice === 'spotify-this-song') {
-            //Liri will provide song information from Spotify based on user request
-            spotifyThis();
+            //Liri will prompt the user for a song choice...
+            inquirer.prompt(spotifyPrompt).then(function(user) {
+                //...then search for info about that song
+                spotifyThis(user.song);
+            });
 
         } else if (choice.userChoice === 'movie-this') {
-            //Liri will provide movie information from OMDB based on user request
-            movieThis();
-
+            //Liri will prompt the user for a movie choice...
+            inquirer.prompt(moviePrompt).then(function(user) {
+                //...then search for info about that movie
+                movieThis(user.movie);
+            });
         } else if (choice.userChoice === 'beautiful-flowers!') {
             //Liri provides "beautiful flowers" for the user who chooses this option
             beautifulFlowers();
 
         } else if (choice.userChoice === 'do-what-it-says') {
-            //Liri takes her commands from the first line of random.txt
+            //Liri takes her first and second (if applicable) prompts from random.txt
             doWhatItSays();
         }
 
@@ -47,77 +52,26 @@ function liri() {
 
 }
 // *************************************** Functions **********************************
+
 function myTweets() {
-    //call to Twitter to get latest tweets from my account.  I don't have 20 yet.
-    client.get('statuses/user_timeline', { screen_name: 'jhornsten', count: 15 }, function(error, tweets, response) {
+    //call to Twitter to get latest 20 tweets from my account.
+    client.get('statuses/user_timeline', { screen_name: 'jhornsten', count: 20 }, function(error, tweets, response) {
         if (error) throw error;
+
         //For each tweet, log its number, when it was created, and the tweet itself 
         for (var i = 0; i < tweets.length; i++) {
-            var text = tweets[i].text;
-            var createdAt = tweets[i].created_at;
-            console.log(i + ". " + createdAt + " : " + text);
-            console.log('___________________________________');
+            var printTweets = (i + ". " + tweets[i].created_at + " : " + tweets[i].text + "\n-------------------------------\n");
+            console.log(printTweets);
             //append this information to log.txt
-            appendToLog(i + ". " + createdAt + " : " + text);
+            appendToLog(printTweets);
         }
         //After each search, user is given the choice to make more requests to Liri, or quit the interface
         askAnotherQuestion();
     });
 }
-//When the user chooses spotify-this-song, s/he is presented with this follow-up prompt.
-var spotifyPrompt = {
-    type: "input",
-    name: "song",
-    message: "What song would you like to know about?",
 
-    //if user doesn't input anything, "The Sign" is the default
-    default: function() {
-        return 'The Sign Ace of Base';
-    }
-}
-
-function spotifyThis() {
-    inquirer.prompt(spotifyPrompt).then(function(user) {
-
-        //Search Spotify for user-specified song.
-
-        var theSong = user.song;
-
-        spotify.search({ type: 'track', query: theSong }, function(err, data) {
-
-
-            if (err) {
-                console.log('Error occurred: ' + err);
-                return;
-            }
-
-            var songInfo = data.tracks.items[0];
-            var songData = {
-                artist: songInfo.artists[0].name,
-                title: songInfo.name,
-                link: songInfo.preview_url,
-                album: songInfo.album.name
-            }
-
-            console.log('***************************************');
-            console.log('Artist: ' + songInfo.artists[0].name);
-            console.log('Song Title: ' + songInfo.name);
-            console.log('Spotify Preview Link: ' + songInfo.preview_url);
-            console.log('Album: ' + songInfo.album.name);
-            console.log('***************************************');
-            appendToLog(songData)
-            askAnotherQuestion();
-
-        });
-    })
-
-};
-
-function beautifulFlowers() {
-    var rickRoll = "Never Gonna Give You Up";
-
-    spotify.search({ type: 'track', query: rickRoll }, function(err, data) {
-
+function spotifyThis(awesomeSong) {
+    spotify.search({ type: 'track', query: awesomeSong }, function(err, data) {
 
         if (err) {
             console.log('Error occurred: ' + err);
@@ -126,105 +80,50 @@ function beautifulFlowers() {
 
         var songInfo = data.tracks.items[0];
 
-        open(songInfo.preview_url);
-        console.log(" ");
-        console.log('!!!!!!!!!!!!!!!!!You just got Rickrolled!!!!!!!!!!!!!!!!!');
-        console.log('!!!!!!!!!!!!!!!!!You just got Rickrolled!!!!!!!!!!!!!!!!!');
-        console.log('!!!!!!!!!!!!!!!!!You just got Rickrolled!!!!!!!!!!!!!!!!!');
-        console.log('**********************************************************');
-        console.log('Artist: ' + songInfo.artists[0].name);
-        console.log('Preview URL ' + songInfo.preview_url);
-        console.log('Album: ' + songInfo.album.name);
-        console.log('**********************************************************');
-        console.log('!!!!!!!!!!!!!!!!!You just got Rickrolled!!!!!!!!!!!!!!!!!');
-        console.log('!!!!!!!!!!!!!!!!!You just got Rickrolled!!!!!!!!!!!!!!!!!');
-        console.log('!!!!!!!!!!!!!!!!!You just got Rickrolled!!!!!!!!!!!!!!!!!');
-        console.log(" ");
-
-        console.log('I\'m *actually* having fun now!  Wait...already over it...');
-
-        appendToLog(songInfo.artists[0].name + "...User Was Rickrolled.");
+        var songFacts = '\n***************************************\n' + 'Artist: ' + songInfo.artists[0].name +
+            '\nSong Title: ' + songInfo.name + '\nSpotify Preview Link: ' + songInfo.preview_url +
+            '\nAlbum: ' + songInfo.album.name + '\n***************************************\n';
+        console.log(songFacts);
+        appendToLog(songFacts);
         askAnotherQuestion();
 
     });
 }
-//When the user chooses movie-this, s/he is presented with this follow-up prompt.
 
-var moviePrompt = {
-    type: "input",
-    name: "movie",
-    message: "What movie would you like to know about?",
-    //if user doesn't input anything, "Mr. Nobody" is the default
-    default: function() {
-        return 'Mr. Nobody';
-    }
-}
+function movieThis(greatMovie) {
 
-var goAgain = {
-    type: 'confirm',
-    name: 'goAgain',
-    message: '...<sigh>...Anything else (hit enter for YES, if you must)?',
-    default: true
-}
+    // Run a request to the OMDB API with the user's movie specified. 
 
-function askAnotherQuestion() {
-    inquirer.prompt(goAgain).then(function(user) {
-        if (user.goAgain) {
-            liri();
-        } else {
-            console.log(' ');
-            console.log('Well, this has been...different. Thanks, I guess? Bye, weirdo...[Liri has disconnected].');
-            console.log(' ');
+    var queryUrl = "http://www.omdbapi.com/?t=" + greatMovie + "&y=&plot=short&tomatoes=true&r=json";
+
+    request(queryUrl, function(error, response, body) {
+
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+
+            // Parse the body of the site and print the movie info
+
+            var movieInfo = "\n********************************************************\n" + "Title: " + JSON.parse(body).Title + "\nYear: " + JSON.parse(body).Year + "\nIMDB Rating: " + JSON.parse(body).imdbRating +
+                "\nCountry: " + JSON.parse(body).Country + "\nLanguage: " + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot +
+                "\nActors: " + JSON.parse(body).Actors + "\nRotten Tomatoes Rating: " + JSON.parse(body).tomatoRating +
+                "\nRotten Tomatoes URL: " + JSON.parse(body).tomatoURL + '\n********************************************************\n';
+
+            console.log(movieInfo);
+            appendToLog(movieInfo);
+            askAnotherQuestion();
         }
-    })
-}
-
-function movieThis() {
-
-    inquirer.prompt(moviePrompt).then(function(user) {
-
-        // Run a request to the OMDB API with the user's movie specified. 
-
-        var queryUrl = "http://www.omdbapi.com/?t=" + user.movie + "&y=&plot=short&tomatoes=true&r=json";
-
-        request(queryUrl, function(error, response, body) {
-
-            // If the request is successful
-            if (!error && response.statusCode === 200) {
-
-                // Parse the body of the site and print the movie info
-
-                var movieData = {
-                    title: JSON.parse(body).Title,
-                    year: JSON.parse(body).Year,
-                    imdbRating: JSON.parse(body).imdbRating,
-                    country: JSON.parse(body).Country,
-                    language: JSON.parse(body).Language,
-                    plot: JSON.parse(body).Plot,
-                    actors: JSON.parse(body).Actors,
-                    rotten_tomatoes_rating: JSON.parse(body).tomatoRating,
-                    rotten_tomatoes_URL: JSON.parse(body).tomatoURL
-                }
-
-                console.log('********************************************************');
-                console.log("Title: " + JSON.parse(body).Title);
-                console.log("Year: " + JSON.parse(body).Year);
-                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-                console.log("Country: " + JSON.parse(body).Country);
-                console.log("Language: " + JSON.parse(body).Language);
-                console.log("Plot: " + JSON.parse(body).Plot);
-                console.log("Actors: " + JSON.parse(body).Actors);
-                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-                console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
-                console.log('********************************************************');
-
-                appendToLog(JSON.stringify(movieData, null, 2));
-                askAnotherQuestion();
-            }
-
-        });
 
     });
+}
+
+function beautifulFlowers() {
+    //Rickrolls the user
+    var rickRoll = "Never Gonna Give You Up";
+    open("https://p.scdn.co/mp3-preview/22bf10aff02db272f0a053dff5c0063d729df988?cid=null");
+    var rickRolled = "\n********************YOU JUST GOT RICKROLLED!!!!!!**************\n*\n*\n*\n*\n*\n*\n*";
+    console.log(rickRolled);
+    appendToLog(rickRolled);
+    spotifyThis(rickRoll);
 }
 
 function doWhatItSays() {
@@ -241,65 +140,25 @@ function doWhatItSays() {
             if (myArray[0] === 'spotify-this-song') {
                 //search for the song specified in myArray[1]  
                 var radSong = myArray[1];
+                spotifyThis(radSong);
 
-                spotify.search({ type: 'track', query: radSong }, function(err, data) {
-
-                    if (err) {
-                        console.log('Error occurred: ' + err);
-                        return;
-                    }
-
-                    var songInfo = data.tracks.items[0];
-
-                    console.log('***************************************');
-                    console.log('Artist: ' + songInfo.artists[0].name);
-                    console.log('Song Title: ' + songInfo.name);
-                    console.log('Spotify Preview Link: ' + songInfo.preview_url);
-                    console.log('Album: ' + songInfo.album.name);
-                    console.log('***************************************');
-                    askAnotherQuestion();
-
-                });
                 //if myArray[0] wants a movie...
             } else if (myArray[0] === 'movie-this') {
                 //search for the movie
                 var coolMovie = myArray[1];
+                movieThis(coolMovie);
 
-                var queryUrl = "http://www.omdbapi.com/?t=" + coolMovie + "&y=&plot=short&tomatoes=true&r=json";
 
-                request(queryUrl, function(error, response, body) {
-
-                    // If the request is successful
-                    if (!error && response.statusCode === 200) {
-
-                        // Parse the body of the site and print the movie info
-
-                        console.log('********************************************************');
-                        console.log("Title: " + JSON.parse(body).Title);
-                        console.log("Year: " + JSON.parse(body).Year);
-                        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-                        console.log("Country: " + JSON.parse(body).Country);
-                        console.log("Language: " + JSON.parse(body).Language);
-                        console.log("Plot: " + JSON.parse(body).Plot);
-                        console.log("Actors: " + JSON.parse(body).Actors);
-                        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-                        console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
-                        console.log('********************************************************');
-
-                    }
-                    askAnotherQuestion();
-                });
-
-                //if myArray wants my tweets, get them
             } else if (myArray[0] === 'my-tweets') {
+                //if myArray wants my tweets, get them
                 myTweets();
-                askAnotherQuestion();
             }
 
         }
 
     })
 }
+
 //function to append all the search results to log.txt
 function appendToLog(info) {
     var f = 'log.txt';
@@ -308,6 +167,49 @@ function appendToLog(info) {
         if (err)
             console.error(err);
     });
+}
+
+var spotifyPrompt = {
+    //When the user chooses spotify-this-song, s/he is presented with this follow-up prompt.
+    type: "input",
+    name: "song",
+    message: "What song would you like to know about?",
+    //if user doesn't input anything, "The Sign" by Ace of Base is the default
+    default: function() {
+        return 'The Sign Ace of Base';
+    }
+}
+
+var moviePrompt = {
+    //When the user chooses movie-this, s/he is presented with this follow-up prompt.
+    type: "input",
+    name: "movie",
+    message: "What movie would you like to know about?",
+    //if user doesn't input anything, "Mr. Nobody" is the default
+    default: function() {
+        return 'Mr. Nobody';
+    }
+}
+
+var goAgain = {
+    //Prompt to find out if user wants to play again before exiting program (default is yes)
+    type: 'confirm',
+    name: 'goAgain',
+    message: '...<sigh>...Anything else (hit enter for YES)?',
+    default: true
+}
+
+function askAnotherQuestion() {
+    //Prompts user to go again, then either restarts or exits program
+    inquirer.prompt(goAgain).then(function(user) {
+        if (user.goAgain) {
+            liri();
+        } else {
+            console.log(' ');
+            console.log('Well, this has been...different. Thanks, I guess? Bye, weirdo...[Liri has disconnected].');
+            console.log(' ');
+        }
+    })
 }
 
 //Start Liri
